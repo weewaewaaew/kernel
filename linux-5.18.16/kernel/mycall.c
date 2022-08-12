c432a6d2ba162bff72cd56a46447c8b27585a4db
weewaewaaew <k5000plus@gmail.com>
2022-08-12 13:51:44 +0900

    hello

---
+++ linux-5.18.16/kernel/mycall.c
@@ -0,0 +1,13 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+#include <linux/syscalls.h>
+int value;
+SYSCALL_DEFINE1(mycall1, int, x){
+	printk("INSERT value: %d", x);
+	value = x;
+	return 0;
+}
+SYSCALL_DEFINE0(mycall0){
+	printk("Current value: %d", value);
+	return value;
+}
